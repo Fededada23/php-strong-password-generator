ad1100a1480fe89f66c1b69470e1616d79c5412e
Fededada23 <dadamofederico@gmail.com>
2023-01-31 18:43:30 +0100

    fixed milestone 3

--- showPw.php
+++ showPw.php
@@ -2,10 +2,10 @@
 require_once "functions.php";
 session_start();
 
-if(!empty($passwordLength)){
-$_SESSION["randomPassword"]=createPassword($passwordLength, $admittedChars, $repeatChars);
-var_dump( $_SESSION["randomPassword"]);
-header("Location:showPw.php");
+if (!empty($passwordLength)) {
+  $_SESSION["randomPassword"] = createPassword($passwordLength, $admittedChars, $repeatChars);
+  var_dump($_SESSION["randomPassword"]);
+  header("Location:showPw.php");
 }
 ?>
 
@@ -24,73 +24,14 @@ header("Location:showPw.php");
 </head>
 
 <body>
-  <div class="container border rounded-4 py-3 px-5">
-    <legend class="fs-1 fw-1">Genera Password:</legend>
-    <form>
-
-      <div class="form-group row">
-        <label for="password-length" class="col-5 col-form-label">Specifica da quanti caratteri deve essere formata la tua password</label>
-        <div class="col-7 ">
-          <input type="number" class="form-control" id="password-length" name="password-lenght" value="<?php echo isset($_GET["password-lenght"]) ? $_GET["password-lenght"] : "" ?>">
-        </div>
-      </div>
-
-      <fieldset class="form-group">
-        <legend class="col-form-label col-sm-2 pt-0 fs-2 fw-2">Preferenze:</legend>
-
-        <div class="row py-3 border-bottom">
-          <legend class="col-form-label col-sm-2 pt-0 ">carattri ammessi:</legend>
-
-          <div class="col-sm-10">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" <?php echo isset($_GET["alphabet"]) ? "checked" : "" ?> name="alphabet" id="alphabet-chars" value="true">
-              <label class="form-check-label" for="alphabet-chars">
-                Lettere
-              </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" <?php echo isset($_GET["integers"]) ? "checked" : "" ?> name="integers" id="integer-chars" value="true">
-              <label class="form-check-label" for="integer-chars">
-                Numeri
-              </label>
-            </div>
-
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" <?php echo isset($_GET["symbols"]) ? "checked" : "" ?> name="symbols" id="symbols-chars" value="true">
-              <label class="form-check-label" for="symbols-chars">
-                Simboli
-              </label>
-            </div>
-          </div>
-        </div>
-      </fieldset>
-      <div class="form-group row  py-3 border-bottom">
-        <div class="col-sm-2">ripetizioni:</div>
-        <div class="col-sm-10">
-          <div class="form-check">
-            <input class="form-check-input" type="radio" id="repeat-true" name="repeat" value="1" <?php echo $repeatChars ? "checked" : "" ?>>
-            <label class="form-check-label" for="repeat-true">
-              si
-            </label>
-          </div>
-
-          <div class="form-check">
-            <input class="form-check-input" type="radio" id="repeat-false" name="repeat" value="0" <?php echo !$repeatChars ? "checked" : "" ?>>
-            <label class="form-check-label" for="repeat-false">
-              no
-            </label>
-          </div>
-        </div>
-      </div>
-      <div class="form-group row py-3">
-        <div class="col-sm-10">
-          <button type="submit" class="btn btn-secondary mx-2"><a href="index.php" class="text-light text-decoration-none">Cancella</a></button>
-          <button type="submit" class="btn btn-primary mx-2">Genera</button>
-        </div>
-      </div>
-    </form>
+  <div class="container d-flex justify-content-center text-center">
+    <div class="w-75 border rounded-4 p-5 ">
+      <h2>La tua nuova password è:</h2>
+      <h1 class="text-primary"><?php echo $_SESSION["randomPassword"] ?></h1>
+    </div>
   </div>
 </body>
+
 </html>
 
 <style>
